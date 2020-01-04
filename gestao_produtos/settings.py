fc63646421e14151b1752d6c637466b60dbc3b02
kelmerpassos <kelmerpassos@gmail.com>
2020-01-04 12:20:49 -0300

    Including ALLWED_HOSTS

--- gestao_produtos/settings.py
+++ gestao_produtos/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1', 'drm-gest-produtos.herokuapp.com'
+]
 
 
 # Application definition
